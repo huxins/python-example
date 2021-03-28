bd402fc0484c99ea9a6d584fd9dfc13b31fa5e5b
huxins <huxins@163.com>
2021-03-28 14:25:28 +0800

    1

--- syy/classification_code.py
+++ syy/classification_code.py
@@ -2,22 +2,26 @@ import xlrd
 import xlwt
 from xlutils.copy import copy
 
-old_excel = xlrd.open_workbook('增值税发票税控开票软件清单信息数据接口.xls', formatting_info=True)
-code_data = old_excel.sheets()[0].col_values(15)[1:]
-new_excel = copy(old_excel)
-row = 1
-col = 15
-ws = new_excel.get_sheet(0)
-
 style = xlwt.XFStyle()
 style.num_format_str = '@'
 font = xlwt.Font()
 font.name = '宋体'
 style.font = font
 
-for x in code_data:
-    if len(x) < 19:
-        ws.write(row, col, label = x.zfill(19), style = style)
-    row+=1
+old_excel = xlrd.open_workbook('test.xls', formatting_info=True)
+new_excel = copy(old_excel)
+col = 15
+count = 0
+for sheet in old_excel.sheets():
+    row = 1
+    code_data = sheet.col_values(15)[1:]
+    ws = new_excel.get_sheet(count)
+    for x in code_data:
+        if type(x) == float and x == int(x):
+            x=str(int(x))
+        if len(x) < 19:
+            ws.write(row, col, label = x.ljust(19,'0'), style = style)
+        row+=1
+    count+=1
 
 new_excel.save('new_code_data.xls')
